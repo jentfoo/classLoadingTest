ecc801ca01277dc04a221fda414d8523764a9fcd
Mike Jensen <jentfoo@gmail.com>
2015-12-17 09:31:37 -0700

    Use ClassLoader rather than Class.forName
    Still does not reproduce the issue :-/

--- src/test/java/com/jentfoo/classloading/LoadClassesTest.java
+++ src/test/java/com/jentfoo/classloading/LoadClassesTest.java
@@ -8,14 +8,8 @@ public class LoadClassesTest {
   public void loadClasses() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
     long start = Clock.accurateForwardProgressingMillis();
     for (int i = 0; i < GenerateClasses.CLASS_COUNT; i++) {
-      @SuppressWarnings("unchecked")
-      Class<? extends AbstractGenerated> c =
-          (Class<? extends AbstractGenerated>) Class.forName(this.getClass().getPackage().getName() +
-                                                               ".generated." +
-                                                               GenerateClasses.CLASS_PREFIX + i);
-      @SuppressWarnings("unused")
-      AbstractGenerated genClass = c.newInstance();
-      genClass.foo();
+      ClassLoader.getSystemClassLoader().loadClass(this.getClass().getPackage().getName() +
+                                                     ".generated." + GenerateClasses.CLASS_PREFIX + i);
     }
     System.out.println("---> run time: " + (Clock.accurateForwardProgressingMillis() - start));
   }
