bcb5018c088394c4f733c5ca5d06f0f30ce68737
Mike Jensen <jentfoo@gmail.com>
2015-12-17 09:35:03 -0700

    Add profiler data

--- src/test/java/com/jentfoo/classloading/LoadClassesTest.java
+++ src/test/java/com/jentfoo/classloading/LoadClassesTest.java
@@ -2,15 +2,20 @@ package com.jentfoo.classloading;
 
 import org.junit.Test;
 import org.threadly.util.Clock;
+import org.threadly.util.debug.Profiler;
 
 public class LoadClassesTest {
   @Test
   public void loadClasses() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+    Profiler p = new Profiler(10);
+    p.start();
     long start = Clock.accurateForwardProgressingMillis();
     for (int i = 0; i < GenerateClasses.CLASS_COUNT; i++) {
       ClassLoader.getSystemClassLoader().loadClass(this.getClass().getPackage().getName() +
                                                      ".generated." + GenerateClasses.CLASS_PREFIX + i);
     }
     System.out.println("---> run time: " + (Clock.accurateForwardProgressingMillis() - start));
+    p.stop();
+    System.out.println(p.dump());
   }
 }
